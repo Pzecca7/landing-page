0a38bd8aecb82a714b421e167cb05dc320b07352
Peter Zecca <pzecca94@gmail.com>
2023-06-22 15:16:03 -0400

    landing-page lab

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use(express.static('public'))
+
+app.listen(4000, console.log(`server running on 4000`))
+
